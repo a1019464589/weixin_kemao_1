2043566cc9444e67a528ad90e6716ea2e5e2abf4
Mr Zeng <Mr Zeng@DESKTOP-SLHHCKD>
2019-03-20 12:37:26 +0800

    初次提交Spirng Boot的项目模板

---
+++ src/main/java/org/a1019464589/weixin/WeixinApplication.java
@@ -0,0 +1,13 @@
+package org.a1019464589.weixin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeixinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeixinApplication.class, args);
+	}
+
+}
